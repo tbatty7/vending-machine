8659447fd7359cdd273a7405c7a160489924aa12
Timothy Batty <tbatty7@gmail.com>
2017-11-06 22:36:41 -0500

    Test for coinIdentifier to identify dimes passed

--- test.js
+++ test.js
@@ -14,6 +14,15 @@ describe('Vending Machine Coding Kata', ()=>{
 			it('should recognize pennies by weight (between 2.5 and 3.11 grams) and diameter (0.75 inches)', ()=>{
 				expect(vendingMachine.coinIdentifier(2.5, 0.75)).to.equal('penny')
 			});
+
+			it('should recognize nickels by weight (5 grams) and diameter (0.835 inches)', ()=>{
+				expect(vendingMachine.coinIdentifier(5, 0.835)).to.equal('nickel')
+			});
+
+			it('should recognize dimes by weight (2.268 grams) and diameter (0.705 inches)', ()=>{
+				expect(vendingMachine.coinIdentifier(2.268, 0.705)).to.equal('dime')
+			});
+
 		});
 	});
 });
--- vendingMachine.js
+++ vendingMachine.js
@@ -7,6 +7,10 @@ VendingMachine.prototype.display = function(){
 VendingMachine.prototype.coinIdentifier = function(weight, diameter){
 	if ((weight >= 2.5 && weight <= 3.11) && (diameter === 0.75)) {
 	return "penny";
+	} else if (weight === 5 && diameter === 0.835) {
+		return "nickel";
+	} else if (weight === 2.268 && diameter === 0.705){
+		return "dime";
 	}
 };
 
