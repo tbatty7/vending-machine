501521131daa3def2713d8e8f725bf590759c31a
Timothy Batty <tbatty7@gmail.com>
2017-11-06 21:50:21 -0500

    Initial test for valid module for vendingMachine passed

---
+++ test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('Vending Machine Coding Kata', ()=>{
+	it('should have a valid module', ()=>{
+		const app = require('./vendingMachine');
+	});
+});
